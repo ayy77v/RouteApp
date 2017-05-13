e24129ca62cca3df402ae3705945c171f902053b
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2017-05-13 16:35:28 +0800

    more routes practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+  	get 'dashboard/main'
+
+  	get 'dashboard/user'
+
+  	get 'dashboard/blog'
+  end
+
   get 'about', to:'pages#about' 
 
-  get 'contact', to: 'pages#contact', as:'lead'
+  get 'contact', to: 'pages#contact', as:"lead"
 
   resources :blogs
+
+  resources :posts
+  
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
